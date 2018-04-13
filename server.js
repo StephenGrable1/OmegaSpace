472aabcb2a443d04d7948fdcc39832d036466f60
Richie Walker <RAW@Richies-MacBook-Pro.local>
2018-04-13 14:07:26 -0400

    add schema, controllers, and server routes

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+const textController = require('./controllers/textController')
+
 
 app.get('/', (req,res) => {
     res.sendFile('');
 })
+
+mongoose.connect('mongodb://OmegaSpace:Codesmith124@ds143099.mlab.com:43099/omegaspace')
+
+
+
+
+
+
+app.get('/test', textController.getText)
+
+app.post('/test', textController.saveText)
+
+console.log('ids = ', textController.ids)
 
 app.listen(5000, () => console.log("Listening to Port: 5000"));
